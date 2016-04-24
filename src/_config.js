687bcf8c75feff2c6474953c2fe23d8bcc3e3c4f
Tomas Vega <tomasvegagalvez@gmail.com>
2016-04-23 20:22:20 -0700

    wooork

--- src/_config.js
+++ src/_config.js
@@ -2,7 +2,8 @@ var config = {};
 
 // mongo uri
 config.mongoURI = {
-  development: "mongodb://localhost/node-stripe-charge",
+  //development: "mongodb://localhost/node-stripe-charge",
+  development: "mongodb://admin:admin@ds019101.mlab.com:19101/heroku_3p84852t",
   test: "mongodb://localhost/node-stripe-charge-test",
   stage: process.env.MONGOLAB_URI
 };
