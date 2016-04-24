2ec630d037ffb02910473ea5efc502e6c197e21f
Tomas Vega <tomasvegagalvez@gmail.com>
2016-04-23 22:30:02 -0700

    config

--- src/_config.js
+++ src/_config.js
@@ -4,8 +4,8 @@ var config = {};
 config.mongoURI = {
   //development: "mongodb://localhost/node-stripe-charge",
   development: "mongodb://admin:admin@ds019101.mlab.com:19101/heroku_3p84852t",
-  //test: "mongodb://localhost/node-stripe-charge-test",
-  test: "mongodb://admin:admin@ds019101.mlab.com:19101/heroku_3p84852t",
+  test: "mongodb://localhost/node-stripe-charge-test",
+  //test: "mongodb://admin:admin@ds019101.mlab.com:19101/heroku_3p84852t",
   stage: process.env.MONGOLAB_URI
 };
 
