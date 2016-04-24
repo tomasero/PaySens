82ca650b4eb374a6770f665832af204f224318af
Tomas Vega <tomasvegagalvez@gmail.com>
2016-04-23 20:00:17 -0700

    db

--- src/server/models/seeds/product.js
+++ src/server/models/seeds/product.js
@@ -8,9 +8,10 @@ var seedProduct = function() {
     if(documents.length === 0){
 
       var prodArry = [
-        {productName: 'Large Shirt', productAmount: 30},
-        {productName: 'Medium Shirt', productAmount: 20},
-        {productName: 'Small Shirt', productAmount: 10}
+        {productName: 'Recarga Máxima', productAmount: 150},
+        {productName: 'Recarga Juerga', productAmount: 100},
+        {productName: 'Recarga General', productAmount: 50},
+        {productName: 'Recarga Misia', productAmount: 10}
       ];
 
       for (var i = 0; i < prodArry.length; i++) {
@@ -18,7 +19,7 @@ var seedProduct = function() {
           {
             name: prodArry[i].productName,
             amount: prodArry[i].productAmount,
-            currency: 'USD',
+            currency: 'Soles',
             forSale: true
           }
         );
